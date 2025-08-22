668ba02afb239d4a75eb0f3bb39b153a84a2e737
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-22 14:54:17 +0430

    search_book function.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from random import choice
 
 def add_book():
     book = {}
@@ -6,6 +7,7 @@ def add_book():
     book['author'] = input("Enter book author: ")
     book['year'] = input("Enter publication year: ")
     book['isbn'] = input("Enter ISBN number: ")
+    
     with open('books.json', 'r+') as file:
         data = json.load(file)
         data['books'].append(book)
@@ -18,6 +20,45 @@ def show_book():
         data = json.load(file)
         for book in data['books']:
             print(f"Title: {book['title']}, Author: {book['author']}, Year: {book['year']}, ISBN: {book['isbn']}")
+            
+def search_book():
+    def menu():
+        print("How do you want to search the book?")
+        print("1. Search by Title")
+        print("2. Search by Author")
+        print("3. Search by Year")
+        print("4. Back to Main Menu")
+        search_option = input("Enter your choice: ").lower()
+        return search_option
+    search_option = menu()
+    with open('books.json', 'r') as file:
+        data = json.load(file)
+        if search_option == '1' or search_option == 'title':
+            title = input("Enter book title: ")
+            for book in data['books']:
+                if book['title'].lower() == title.lower():
+                    results = [book]
+        elif search_option == '2' or search_option == 'author':
+            author = input("Enter book author: ")
+            for book in data['books']:
+                if book['author'].lower() == author.lower():
+                    results = [book]
+        elif search_option == '3' or search_option == 'year':
+            year = input("Enter publication year: ")
+            for book in data['books']:
+                if book['year'] == year:
+                    results = [book]
+        elif search_option == '4':
+            return
+        else:
+            print("Invalid option.")
+            return
+
+        if results:
+            for book in results:
+                print(f"Title: {book['title']}, Author: {book['author']}, Year: {book['year']}, ISBN: {book['isbn']}")
+        else:
+            print("No books found.")
 
 def menu():
     print("Welcome to the Smart Library")
