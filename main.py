f036095e1b392238cc9e56c19d1cd235e1b06a44
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-22 14:34:25 +0430

    add menu function.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import json 
+import json
+
+def menu():
+    print("Welcome to the Smart Library")
+    print("1. Add Book")
+    print("2. Remove Book")
+    print("3. Search Book")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+    return choice
